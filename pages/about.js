f2646b4e586e550822b93db3e6077a11054c43fe
Jessica Leverett <Jessak1202@gmail.com>
2021-09-15 14:04:05 -0500

    main

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from "next/head";
+
 const About = () => {
-    return (
-        <div>
-            <h1> About </h1>
-            <p> Lorem psum gost soi lor yo thr al j. </p>
-        </div>
-    );
-}
- 
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About </title>
+        <meta name="keywords" content="Frontend development" />
+      </Head>
+      <div>
+        <h1> About </h1>
+        <p> Lorem psum gost soi lor yo thr al j. </p>
+      </div>
+    </>
+  );
+};
+
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,25 @@
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.title}>
-      <h1> Homepage Next</h1>
-      <p className={styles.text}>
-        Lorem ipdsum oiejr learning osm e new awesome react framework called
-        NEXT js IT IS basically the most amazing fullstack framework I have ever
-        used. Without a doubt.
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}> See Ninja Listing </a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home </title>
+        <meta name="keywords" content="Frontend development" />
+      </Head>
+      <div>
+        <h1 className={styles.title}> Homepage Next</h1>
+        <p className={styles.text}>
+          Lorem ipdsum oiejr learning osm e new awesome react framework called
+          NEXT js IT IS basically the most amazing fullstack framework I have
+          ever used. Without a doubt.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}> See Ninja Listing </a>
+        </Link>
+      </div>
+    </>
   );
 }
