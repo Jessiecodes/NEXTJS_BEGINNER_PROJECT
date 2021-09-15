570430037f65d634591c3df9838cd1ac5121890d
Jessica Leverett <Jessak1202@gmail.com>
2021-09-15 13:43:16 -0500

    first commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1> About </h1>
+            <p> Lorem psum gost soi lor yo thr al j. </p>
+        </div>
+    );
+}
+ 
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,9 @@
+const Ninjas = () => {
+    return (
+        <div>
+            <h1>All Ninjas Nexting it up</h1>
+        </div>
+      );
+}
+ 
+export default Ninjas;
