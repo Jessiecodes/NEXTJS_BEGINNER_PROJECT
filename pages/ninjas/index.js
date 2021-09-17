8af87a29cb531fd5b2dd43734ffddb0231ae3294
Jessica Leverett <Jessak1202@gmail.com>
2021-09-17 06:16:26 -0500

    Fetching data from API

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,28 @@
-const Ninjas = () => {
-    return (
-        <div>
-            <h1>All Ninjas Nexting it up</h1>
-        </div>
-      );
-}
- 
+import styles from "../../styles/Ninjas.module.css";
+import Link from "next/link";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Ninjas = ({ ninja }) => {
+  return (
+    <div>
+      <h1>All Ninjas Nexting it up</h1>
+      {ninja.map((ninja) => (
+        <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
+          <a className={styles.single}>
+            <h3> {ninja.name} </h3>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
 export default Ninjas;
